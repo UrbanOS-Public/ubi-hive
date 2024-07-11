19e8e7d0442a8200c911cf0b85e54e35fa11c498
Ian Abbott <ian.j.abbott@accenture.com>
2024-07-11 18:16:56 -0500

    remove echo from get_image_tag

--- get_image_tag.sh
+++ get_image_tag.sh
@@ -4,8 +4,6 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 BUILD_NUM_FILE="${SCRIPT_DIR}/image_build_num.txt"
 BUILD_DESCRIPTOR_FILE="${SCRIPT_DIR}/image_build_descriptor.txt"
 
-echo $BUILD_NUM_FILE
-
 if [[ -s ${BUILD_NUM_FILE} ]]
 then
     BUILD_NUM=$(head -1 ${BUILD_NUM_FILE} | tr -d '\n')
@@ -21,5 +19,5 @@ else
     echo "The build descriptor file is missing or empty."
     exit -1
 fi
-echo ${BUILD_DESCRIPTOR}
+
 echo "$(${SCRIPT_DIR}/get_metastore_version.sh)-${BUILD_DESCRIPTOR}-${BUILD_NUM}"
